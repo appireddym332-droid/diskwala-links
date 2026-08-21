011d79996dbf0834bf90b79f34367523c301dc6c
appireddym332-droid <appireddym332@gmail.com>
2026-08-21 22:34:52 +0530

    Update MainActivity.java

--- app/src/main/java/com/diskwalalinks/app/MainActivity.java
+++ app/src/main/java/com/diskwalalinks/app/MainActivity.java
@@ -1,15 +1,13 @@
 package com.diskwalalinks.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private WebView webView;
 
@@ -22,88 +20,71 @@ public class MainActivity extends AppCompatActivity {
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
-        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
 
         webView.setWebViewClient(new WebViewClient() {
 
             @Override
             public boolean shouldOverrideUrlLoading(
-                    WebView view,
-                    WebResourceRequest request) {
+                    WebView view, String url) {
 
-                return handleUrl(view, request.getUrl().toString());
-            }
+                if (url == null) {
+                    return false;
+                }
 
-            @Override
-            public boolean shouldOverrideUrlLoading(
-                    WebView view,
-                    String url) {
+                // Normal website links
+                if (url.startsWith("http://")
+                        || url.startsWith("https://")) {
+                    return false;
+                }
 
-                return handleUrl(view, url);
+                // Diskwala / intent links
+                if (url.startsWith("intent://")) {
+
+                    try {
+                        Intent intent = Intent.parseUri(
+                                url,
+                                Intent.URI_INTENT_SCHEME
+                        );
+
+                        if (intent.resolveActivity(
+                                getPackageManager()) != null) {
+
+                            startActivity(intent);
+                            return true;
+                        }
+
+                        // Do NOT open Play Store
+                        return true;
+
+                    } catch (Exception e) {
+                        return true;
+                    }
+                }
+
+                // Other app links
+                try {
+                    Intent intent = new Intent(
+                            Intent.ACTION_VIEW,
+                            Uri.parse(url)
+                    );
+
+                    if (intent.resolveActivity(
+                            getPackageManager()) != null) {
+
+                        startActivity(intent);
+                    }
+
+                } catch (Exception e) {
+                    // Ignore
+                }
+
+                return true;
             }
         });
 
-        webView.loadUrl("https://appireddym332-drops.github.io/diskwala-links/");
-    }
-
-    private boolean handleUrl(WebView view, String url) {
-
-        if (url == null) {
-            return false;
-        }
-
-        // Normal web links
-        if (url.startsWith("http://") || url.startsWith("https://")) {
-            return false;
-        }
-
-        // Open intent links directly in the installed app
-        if (url.startsWith("intent://")) {
-
-            try {
-                Intent intent = Intent.parseUri(
-                        url,
-                        Intent.URI_INTENT_SCHEME
-                );
-
-                // Try opening the target app directly
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                    return true;
-                }
-
-                // If app is not installed, use the web fallback
-                Uri data = intent.getData();
-
-                if (data != null &&
-                        (data.toString().startsWith("http://")
-                        || data.toString().startsWith("https://"))) {
-
-                    view.loadUrl(data.toString());
-                    return true;
-                }
-
-            } catch (Exception e) {
-                // Do nothing - don't open Play Store
-            }
-
-            return true;
-        }
-
-        // Other app links
-        try {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse(url));
-
-            if (intent.resolveActivity(getPackageManager()) != null) {
-                startActivity(intent);
-            }
-
-        } catch (Exception e) {
-            // Ignore
-        }
-
-        return true;
+        webView.loadUrl(
+                "https://appireddym332-droid.github.io/diskwala-links/"
+        );
     }
 
     @Override
